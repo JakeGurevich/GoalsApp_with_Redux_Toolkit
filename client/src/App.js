f6fec87dcd56f4da87fcc400a56cb1cb555b6fe1
Jake Gurevich <umanrh@gmail.com>
2022-03-20 10:00:58 +0200

    project init

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
-const App = () => {
-	return <div>APP</div>;
-};
+function App() {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
+  )
+}
 
-export default App;
+export default App
---
+++ server/db/configDB.js
@@ -0,0 +1,23 @@
+const mongoose=require ("mongoose");
+const Colors = require("colors");
+
+const connectDB = async () => {
+
+  
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI
+      //    {
+      //   useNewUrlParser: true,
+      //   useCreateIndex: true,
+      //   useFindAndModify: false,
+      //   useUnifiedTopology: true,
+      // }
+      );
+      console.log(`MongoDB connected to ${conn.connection.host}`.cyan.underline);
+    } catch (error) {
+      console.error(`MongoDB connection error: ${error}`.red.underline.bold);
+      process.exit(1);
+    }
+  };
+
+  module.exports = connectDB
